7a08d769f88ea2ead93c86b97b47cbb4b5f0d1d7
Marcus <loderm14@my.lanecc.edu>
2018-10-22 12:04:36 -0600

    commit

---
+++ Lab4-MarcusLoder-PigGame/MainActivity.cs
@@ -0,0 +1,188 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Views;
+
+namespace Lab4_PigGame_MarcusLoder
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        //instance variables
+        Button rollButton;
+        Button endTurnButton;
+        Button newGameButton;
+
+        EditText p1NameEditText;
+        EditText p2NameEditText;
+
+        TextView p1ScoreTextView;
+        TextView p2ScoreTextView;
+        TextView pointsThisTurnTextView;
+        TextView playersTurnTextView;
+
+        //create game
+        PigGame game;
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            // Define Controls
+            rollButton = FindViewById<Button>(Resource.Id.rolldice);
+            endTurnButton = FindViewById<Button>(Resource.Id.endTurn);
+            newGameButton = FindViewById<Button>(Resource.Id.newGame);
+
+            pointsThisTurnTextView = FindViewById<TextView>(Resource.Id.ptsThisTurn);
+            playersTurnTextView = FindViewById<TextView>(Resource.Id.whosTurn);
+
+            p1ScoreTextView = FindViewById<TextView>(Resource.Id.score1);
+            p2ScoreTextView = FindViewById<TextView>(Resource.Id.score2);
+
+            var diceImageView = FindViewById<ImageView>(Resource.Id.dice);
+
+            var player1NameEditText = FindViewById<EditText>(Resource.Id.p1Name);
+            var player2NameEditText = FindViewById<EditText>(Resource.Id.p2Name);
+
+            //instantiate a new game.
+            game = new PigGame();
+
+            // The click EVENT (Who know Will Smith could Switch)
+            rollButton.Click += delegate
+            {
+                //Update Names
+                if (p1NameEditText.Text == "")
+                {
+                    game.Player1.name = "Player 1";
+                }
+                else game.Player1.name = p1NameEditText.Text;
+
+                if (p2NameEditText.Text == "")
+                {
+                    game.Player2.name = "Player 2";
+                }
+                else game.Player2.name = p2NameEditText.Text;
+
+                //Update Whos Turn It Is
+                if (game.Player1Trn)
+                {
+                    playersTurnTextView.Text = game.Player1.name + "'s Turn";//Player 1
+                }
+                else
+                {
+                    playersTurnTextView.Text = game.Player2.name + "'s Turn";//Player 2
+                }
+
+                int roll = game.rolldice();
+                switch (roll)
+                {
+                    case 1:
+                        diceImageView.SetImageResource(Resource.Drawable.Dice - 1);
+                        break;
+                    case 2:
+                        diceImageView.SetImageResource(Resource.Drawable.Dice - 2);
+                        break;
+                    case 3:
+                        diceImageView.SetImageResource(Resource.Drawable.Dice - 3);
+                        break;
+                    case 4:
+                        diceImageView.SetImageResource(Resource.Drawable.Dice - 4);
+                        break;
+                    case 5:
+                        diceImageView.SetImageResource(Resource.Drawable.Dice - 5);
+                        break;
+                    case 6:
+                        diceImageView.SetImageResource(Resource.Drawable.Dice - 6);
+                        break;
+                    default:
+                        break;
+                }
+
+                int turnPts = game.refreshTurnPts(roll);
+
+                if (turnPts == 0)
+                {
+                    rollButton.Enabled = false;
+                    Android.Widget.Toast.MakeText(this, "You Lost Your Points!", Android.Widget.ToastLength.Short).Show();
+                    pointsThisTurnTextView.Text = "0";
+                }
+                else
+                {
+                    pointsThisTurnTextView.Text = turnPts.ToString();//Update turn Pts
+                }
+            };
+
+            // End Game Click Event Handler
+            endTurnButton.Click += delegate
+            {
+                rollButton.Enabled = true;
+
+                if (game.MatchPoint)
+                {
+
+                    if (game.Player1Trn)
+                    {
+                        p1ScoreTextView.Text = game.RefreshPlayersScore().ToString();
+                    }
+                    else
+                    {
+                        p2ScoreTextView.Text = game.RefreshPlayersScore().ToString();
+                    }
+
+                    if (game.player1Won())
+                    {
+                        //P1 won
+                        Android.Widget.Toast.MakeText(this, "Player 1 Wins--Reseting Game", Android.Widget.ToastLength.Short).Show();
+
+                    }
+                    else
+                    {
+                        //P2 won
+                        Android.Widget.Toast.MakeText(this, "Player 2 Wins--Reseting Game", Android.Widget.ToastLength.Short).Show();
+                    }
+                    reset();
+                }
+                else
+                {
+                    if (game.Player1Trn)
+                    {
+                        p1ScoreTextView.Text = game.RefreshPlayersScore().ToString();
+                        playersTurnTextView.Text = game.Player2.name + "'s Turn";
+                    }
+                    else
+                    {
+                        p2ScoreTextView.Text = game.RefreshPlayersScore().ToString();
+                        playersTurnTextView.Text = game.Player1.name + "'s Turn";
+                    }
+
+                    pointsThisTurnTextView.Text = "0";
+                    game.Player1Trn = !game.Player1Trn;
+                }
+
+                newGameButton.Click += delegate
+                {
+                    //reset the ui
+                    Android.Widget.Toast.MakeText(this, "Reseting Game", Android.Widget.ToastLength.Short).Show(); p1ScoreTextView.Text = game.refreshPlayersScore().ToString();//update score
+                    reset();
+                };
+
+            };
+        }
+        protected void update()
+        {
+
+        }
+        private void reset()
+        {
+            game.reset();
+            playersTurnTextView.Text = "Player 1's Turn";
+            p1ScoreTextView.Text = game.Player1.score.ToString();//Reset Score
+            p2ScoreTextView.Text = game.Player2.score.ToString();//Reset Score
+            pointsThisTurnTextView.Text = "0"; //Reset the Text View
+        }
+    }
+}
---
+++ Lab4-MarcusLoder-PigGame/PigGame.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace Lab4_PigGame_MarcusLoder
+{
+    class PigGame
+    {
+        //create a player
+        public class player
+        {
+            public string name { get; set; }
+            public int score { get; set; }
+            public int turnScore { get; set; }
+            public bool win { get; set; }
+
+            public player()
+            {
+                name = "";
+                score = 0;
+                turnScore = 0;
+                win = false;
+            }
+        }
+
+        //winning score
+        const int WINNINGSCORE = 100;
+
+        //Create and instantiate new players
+        public player Player1 = new player();
+        public player Player2 = new player();
+
+        //Keeps track of turns
+        public bool Player1Trn { get; set; } = true;
+
+        //Random for the dice roll
+        public Random random = new Random();
+
+        //Ending bool
+        public bool MatchPoint { get; set; } = false;
+
+        //Random Dice Roller
+        public int rolldice()
+        {
+            return random.Next(6) + 1;
+        }
+
+        //updates the players scores
+        public int RefreshPlayersScore()
+        {
+            if (Player1Trn)
+            {
+                Player1.score += Player1.turnScore;
+                Player1.turnScore = 0;
+
+                if (Player1.score >= WINNINGSCORE)
+                {
+                    MatchPoint = true;
+                }
+                return Player1.score;
+            }
+            else
+            {
+                Player2.score += Player2.turnScore;
+
+                if (Player2.score >= WINNINGSCORE)
+                {
+                    Player2.turnScore = 0;
+                }
+                return Player2.score;
+            }
+        }
+
+        public int refreshTurnPts(int score)
+        {
+            if (Player1Trn)
+            {
+                if (score == 1)
+                {
+                    Player1.turnScore = 0;
+                }
+                else
+                {
+                    Player1.turnScore += score;
+                }
+                return Player1.turnScore;
+            }
+            else
+            {
+                if (score == 1)
+                {
+                    Player2.turnScore = 0;
+                }
+                else
+                {
+                    Player2.turnScore += score;
+                }
+                return Player2.turnScore;
+            }
+        }
+
+        public bool checkWin()
+        {
+            if (Player1Trn)
+            {
+                Player1.score += Player1.turnScore;
+                Player1.turnScore = 0;
+
+                if (Player1.score >= WINNINGSCORE)
+                {
+                    Player1.win = true;
+                }
+                return Player1.win;
+            }
+            else
+            {
+                Player2.score += Player2.turnScore;
+                Player2.turnScore = 0;
+
+                if (Player2.score >= WINNINGSCORE)
+                {
+                    Player2.win = true;
+                }
+                return Player2.win;
+            }
+        }
+
+        public bool player1Won()
+        {
+            if (Player1.score > Player2.score)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void reset()
+        {
+            Player1.score = 0;
+            Player1.turnScore = 0;
+            Player1.win = false;
+            Player2.score = 0;
+            Player2.turnScore = 0;
+            Player2.win = false;
+            Player1Trn = true;
+            MatchPoint = false;
+        }
+    }
+}
